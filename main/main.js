74c126ac8ec1888e304709d7ebc2031bdf55e915
guoyangnice <yangjienice@126.com>
2019-08-25 11:27:41 +0800

    all pass

--- main/main.js
+++ main/main.js
@@ -1,9 +1,25 @@
 // Write your cade below:
-function caclRemaindar() {};
+function caclRemaindar(number1,number2) {
+    return number1 % number2;
+};
 
-function caclSum() {};
+function caclSum(array) {
+    let result = 0;
+    for(let i = 0 ; i< array.length;i++){
+        result += array[i];
+    }
+    return result;
+};
 
-function caclSumInConditon() {};
+function caclSumInConditon(array,number) {
+    let result = 0;
+    for(let i = 0; i< array.length;i++){
+        if(array[i] < number){
+            result += array[i];
+        }
+    }
+    return result;
+};
 
 module.exports = {
     caclRemaindar,
